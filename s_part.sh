055d5ba79b44622437c748900424b6aafefbd8aa
Danila Gornushko <me@okhsunrog.ru>
2022-11-16 03:28:39 +0300

    test 4

--- s_part.sh
+++ s_part.sh
@@ -182,7 +182,8 @@ sleep 1
 zfs set canmount=off zroot/data/home
 sleep 1
 zfs set canmount=on zroot/data/home
-pkill zed
+PID_ZED=`pidof zed`
+kill $PID_ZED
 sed -Ei "s|/mnt/install/?|/|" /etc/zfs/zfs-list.cache/*
 zgenhostid $(hostid)
 mkinitcpio -p linux-okhsunrog
